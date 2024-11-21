1d4504753ad074b4356f1c927cf5b2d0512656c2
techLeopard <adilet.satylganov@gmail.com>
2024-11-21 10:55:10 +0600

    added JUnitTests

---
+++ src/test/java/JUnitTests.java
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.*;
+
+public class JUnitTests {
+
+    @BeforeAll
+    static void beforeAll(){
+        System.out.println("Here is beforeAll()");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("Here is beforeEach()");
+    }
+
+    @AfterAll
+    static void afterAll(){
+        System.out.println("Here is afterAll()");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("Here is afterEach()");
+    }
+
+    @Test
+    void firstTest(){
+        System.out.println("Here is the first test!");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("Here is the second test!");
+    }
+}
