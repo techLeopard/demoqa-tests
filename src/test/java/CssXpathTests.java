852368b33bdc1a22557929fd3d74e928c4089b80
techLeopard <adilet.satylganov@gmail.com>
2024-11-21 12:58:15 +0600

    added CssXpathTests

---
+++ src/test/java/CssXpathTests.java
@@ -0,0 +1,50 @@
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class CssXpathTests {
+
+
+    void cssXpathExamples(){
+
+        // <input type="email" class="inputtext login_form_input_box" name="email" id="userEmail" data-testid="email">
+        $("[data-testid=email]").setValue("1");
+        $(by("data-testid", "email")).setValue("1");
+
+        // <input type="email" class="inputtext login_form_input_box" name="email" id="userEmail">
+        $("[id=email]").setValue("1");
+        $("#email").setValue("1");
+        $("input#email").setValue("1");
+        $x("//*[@id='email']").setValue("1");
+
+        // <input type="email" class="inputtext login_form_input_box" name="email">
+        $("[name=email]").setValue("1");
+        $(byName("email")).setValue("1");
+
+        // <input type="email" class="inputtext login_form_input_box">
+        $("class=login_form_input_box").setValue("1");
+        $(".login_form_input_box").setValue("1");
+        $(".inputtext.login_form_input_box").setValue("1");
+        $("input .inputtext .login_form_input_box").setValue("1");
+        $("//input[@class='inputtext'][@class='login_form_input_box']").setValue("1");
+
+        // <div class="inputtext">
+        //      <input type="email" class="login_form_input_box"></input>
+        // </div>
+        $(".inputtext .login_form_input_box").setValue("1");
+        $(".inputtext").$(".login_form_input_box").setValue("1");
+
+
+        // <div>Hello, qa guru!</div>
+        $x("//div[text()='Hello, qa guru!']").click();
+        $(byText("Hello, qa guru!")).click();
+        $(withText("lo, qa gu")).click();
+
+
+
+
+    }
+
+
+
+}
